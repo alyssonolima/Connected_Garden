903ad6bc17634dcf285891ae89076d98b1498925
Alysson <Alysson@PC>
2015-10-24 17:31:37 -0300

    Alerts

--- Connected_Garden/src/br/com/connectedgarden/dao/AlertsDao.java
+++ Connected_Garden/src/br/com/connectedgarden/dao/AlertsDao.java
@@ -1,5 +1,47 @@
 package br.com.connectedgarden.dao;
 
-public class AlertsDao {
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import br.com.connectedgarden.entity.Alert;
+
+public class AlertsDao {
+	
+	private JdbcTemplate jt;
+	
+	public List<Alert> findAlerts(Integer iduser) throws SQLException{
+		
+		StringBuilder sql = new StringBuilder(
+			" SELECT alert.ID_ALERT, alert.ID_POT, alert.ID_ALERT_TYPE, alert.DESCRIPTION, alert.ACTIVATION_DATE, alert.ACTIVE " +
+			" FROM connected_garden.alert alert " +
+			" INNER JOIN connected_garden.pot pot on pot.ID_POT = alert.ID_POT " +
+			" INNER JOIN connected_garden.garden garden on garden.ID_GARDEN = pot.ID_GARDEN " +
+			" INNER JOIN connected_garden.user_garden us on us.ID_USER = garden.ID_USER " +
+			" where us.ID_USER = " + iduser
+		);
+		jt = new JdbcTemplate(Fabrica.getDataSource());
+		List<Map<String, Object>> result = jt.queryForList(sql.toString());
+		List<Alert> alerts = new ArrayList<Alert>();
+		
+		for(Map<String, Object> map: result){
+			Alert alert = new Alert();
+			
+			alert.setId((int) map.get("id_alert"));
+			alert.setIdPot((int) map.get("id_pot"));
+			alert.setIdAlertType((int) map.get("id_alert_type"));
+			alert.setDescription((String) map.get("description"));
+			alert.setActivationDate((Date) map.get("activation_date"));
+			Boolean active = (Boolean) map.get("active");
+			alert.setActive(active);
+			
+			alerts.add(alert);
+		}
+		
+		return alerts;
+	}
 }
--- Connected_Garden/src/br/com/connectedgarden/jsf/AlertsMB.java
+++ Connected_Garden/src/br/com/connectedgarden/jsf/AlertsMB.java
@@ -1,7 +1,13 @@
 package br.com.connectedgarden.jsf;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+
+import br.com.connectedgarden.dao.AlertsDao;
+import br.com.connectedgarden.entity.Alert;
 
 @ManagedBean
 @SessionScoped
@@ -9,7 +15,15 @@ public class AlertsMB extends SuperMB{
 
 	public static final String ALERTS = "/alerts.jsf";
 	
+	private AlertsDao dao;
+	
 	public String iniciar(){
 		return ALERTS;
 	}
+	
+	public List<Alert> findAlerts() throws SQLException{
+		dao = new AlertsDao();		
+		return dao.findAlerts(getUser().getId()); 
+	}
+	
 }
